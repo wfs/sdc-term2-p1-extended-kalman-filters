f7c92dd8a0a537a09d3f61b91dc89a35e9cadb72
andrew <hullpod.central@gmail.com>
2017-04-20 21:00:11 +1000

    BDD-style test of tools methods.

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "tools.h"
 
+using namespace std;
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
@@ -11,15 +12,83 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    /**
+    TODO:
+      * Calculate the RMSE here.
+    */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    // ... your code here
+    if (estimations.size() != ground_truth.size()
+        || estimations.size() == 0) {
+        cout << "Invalid estimation or ground_truth data" << endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for (int i = 0; i < estimations.size(); ++i) {
+        // ... your code here
+
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        //coefficient-wise multiplication
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    // ... your code here
+    rmse = rmse / estimations.size();
+
+    //calculate the squared root
+    // ... your code here
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
+    /**
+    TODO:
+      * Calculate a Jacobian here.
+    */
+
+    /**
+     * z-state : 3 rows of measurement ; Range 'rho', Bearing 'phi', Range Rate 'rho dot'.
+     * x-state : 4 cols of prediction ; Position Px, Py & Velocity Vx, Vy.
+     */
+    MatrixXd Hj(3, 4);
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    //YOUR CODE HERE
+    //pre-compute a set of terms to avoid repeated calculation
+    /**
+     * c3 = c1^1 * c2^0.5 = c3^1.5 <-> c3^3/2
+     * See algebra Laws of Exponents - http://www.mathsisfun.com/algebra/exponent-laws.html
+     */
+    float c1 = px * px + py * py;
+    float c2 = sqrt(c1);
+    float c3 = (c1 * c2);
+
+    //check division by zero
+    if (fabs(c1) < 0.0001) {
+        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+        return Hj;
+    }
+
+    //compute the Jacobian matrix
+    Hj << (px / c2), (py / c2), 0, 0,
+            -(py / c1), (px / c1), 0, 0,
+            py * (vx * py - vy * px) / c3, px * (px * vy - py * vx) / c3, px / c2, py / c2;
+
+    return Hj;
 }
